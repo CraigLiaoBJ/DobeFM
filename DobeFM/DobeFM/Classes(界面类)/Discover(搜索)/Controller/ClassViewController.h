73e5be556953a3a80a867b594dd849b4f864da8c
lanou <985595975@qq.com>
2015-07-17 10:52:30 +0800

    主页面7-17   10：52

---
+++ DobeFM/DobeFM/Classes(界面类)/Discover(搜索)/Controller/ClassViewController.h
@@ -0,0 +1,13 @@
+//
+//  ClassViewController.h
+//  DobeFM
+//
+//  Created by lanou3g on 15/7/17.
+//  Copyright (c) 2015年 DobeFM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ClassViewController : UIViewController
+
+@end
